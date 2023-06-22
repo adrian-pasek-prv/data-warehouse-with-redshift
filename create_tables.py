746849e8e4462bede72627fa94e7db7299abf89c
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-06-22 05:56:57 +0000

    Remove terraform variables import

--- create_tables.py
+++ create_tables.py
@@ -1,22 +1,8 @@
+import configparser
 import psycopg2
-import json
-import os
 from sql_queries import create_table_queries, drop_table_queries
 
-# Get the home path string
-home = os.path.expanduser('~')
 
-# Import variables from terraform.tvars.json
-with open(home + '/github-repos/de-with-aws/terraform/terraform.tvars.json') as f:
-    data = json.load(f)
-    DWH_DB = data['redshift_database_name']
-    DWH_DB_USER = data['redshift_admin_username']
-    DWH_DB_PASSWORD = data['redshift_admin_password']
-
-# Import outputs from terraform_output.json
-with open(home + '/github-repos/de-with-aws/terraform/terraform_output.json') as f:
-    data = json.load(f)
-    DWH_ENDPOINT = data['cluster_endpoint']['value']
 
 def drop_tables(cur, conn):
     for query in drop_table_queries:
@@ -31,8 +17,10 @@ def create_tables(cur, conn):
 
 
 def main():
+    config = configparser.ConfigParser()
+    config.read('dwh.cfg')
 
-    conn = psycopg2.connect(f"host={DWH_ENDPOINT} dbname={DWH_DB} user={DWH_DB_USER} password={DWH_DB_PASSWORD}")
+    conn = psycopg2.connect("host={} dbname={} user={} password={} port={}".format(*config['CLUSTER'].values()))
     cur = conn.cursor()
 
     drop_tables(cur, conn)
