5603aa2a398eb7d41a404e7743f3c7e1e497832a
adrian-pasek-prv <pasek.adrian.314@gmail.com>
2023-06-27 07:29:11 +0000

    Add documentation to functions

--- create_tables.py
+++ create_tables.py
@@ -5,18 +5,27 @@ from sql_queries import create_table_queries, drop_table_queries
 
 
 def drop_tables(cur, conn):
+    '''
+    Iterate over list of DROP statements and drop existing tables
+    '''
     for query in drop_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def create_tables(cur, conn):
+    '''
+    Iterate over list of CREATE statements and create tables
+    '''
     for query in create_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Read config file, establish connection to Redshift and execute SQL statements, finally close the connection
+    '''
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
--- etl.py
+++ etl.py
@@ -4,18 +4,28 @@ from sql_queries import copy_table_queries, insert_table_queries
 
 
 def load_staging_tables(cur, conn):
+    '''
+    Iterate over COPY statements and load staging tables into Redshift
+    '''
     for query in copy_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def insert_tables(cur, conn):
+    '''
+    Iterate over INSERT statements and insert data from staging tables into fact and dimension tables
+    in Redshift
+    '''
     for query in insert_table_queries:
         cur.execute(query)
         conn.commit()
 
 
 def main():
+    '''
+    Read config file, establish connection to Redshift and execute load and insert statements in Redshift
+    '''
     config = configparser.ConfigParser()
     config.read('dwh.cfg')
 
